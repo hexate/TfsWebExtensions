b0c001933d9b245c27889400e0fcfdc666b55447
Blake Davis <me@blakejay.com>
2013-07-01 12:59:44 -0700

    update 2 fixes

--- TaskBoard/TfsWebExtensions.TaskBoard.debug.js
+++ TaskBoard/TfsWebExtensions.TaskBoard.debug.js
@@ -1,8 +1,19 @@
-TFS.module("TfsWebExtensions.TaskBoard",["TFS.Host", "TFS.Admin", "TFS.WorkItemTracking", "TFS.Agile.TaskBoard"], function () {
+define([
+	"require",
+	"exports",
+	"Presentation/Scripts/TFS/TFS",
+	"Presentation/Scripts/TFS/TFS.Core",
+	"Presentation/Scripts/TFS/TFS.Host",
+	"Presentation/Scripts/TFS/TFS.OM",
+	"Presentation/TestScripts/Resources/TFS.Resources.Common",
+	"Presentation/Scripts/TFS/TFS.UI.Controls",
+	"Presentation/Scripts/TFS/TFS.UI.Controls.Menus",
+	"WorkItemTracking/Scripts/TFS.WorkItemTracking"
+], function (require, exports, tfs, Core, TfsHost, TfsOm, CommonResources, Controls, MenuControls, WorkItemTracking) { 
 	var moduleBaseUrl = TFS.getModuleBase("TfsWebExtensions.TaskBoard");
 	var digits = /\d+/;
 	var wIds = $('[id^="tile-"]').map(function() { return digits.exec(this.id)[0]; }).get();
-	var wim = TFS.WorkItemTracking.WorkItemStore.getDefault().workItemManager;
+	var wim = TFS.OM.TfsTeamProjectCollection.getDefaultConnection().getService(TFS.WorkItemTracking.WorkItemStore).workItemManager;
 	
 	function isBoard() {
 		return TFS.Host.TfsContext.getDefault().navigation.currentController == "boards";
